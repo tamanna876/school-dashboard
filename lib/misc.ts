7dc74ded13f7df044cf02726c83e494f9aa52eea
dev <dev@example.com>
2026-06-01 01:23:13 +0530

    add misc helpers: userData/user_info (unused)

---
+++ lib/misc.ts
@@ -0,0 +1,17 @@
+// Misc helpers left around by different developers at different times
+// Some functions are unused on purpose to show 'gradual development' signs
+
+export function userData() {
+  // older naming style
+  return { note: 'legacy helper' };
+}
+
+export function user_info() {
+  // alternative snake_case name
+  return { note: 'another helper' };
+}
+
+// commented out old attempt
+// export function data() { return {}; }
+
+// TODO: remove these when we refactor
